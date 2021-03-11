d68674c06ce670fb054473fa17abf95894e8c1b5
Sushmita Sahu <itzcutiesush@gmail.com>
2021-03-12 00:40:00 +0530

    added logic & styling

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log("script loaded");
+let parent = document.getElementsByClassName('cube-container')[0];
+let childNodeList = [];
+let nodes = parent.childNodes;
+
+function getRandomFromRange(min, max)
+{ 
+    let prevList = [];
+    function x(){
+        let newNum = Math.floor(Math.random() * (max - min + 1) + min);
+        if(prevList.indexOf(newNum) == -1){
+            prevList.push(newNum);
+            return newNum;
+        }
+
+        return x();
+    };
+    return x;
+}
+
+function shuffleCubes(){
+    let randomNum = getRandomFromRange(1, 9);
+    for(let i = 0; i<nodes.length; i++){
+        if(nodes[i].className?.includes('cube')){
+            let itemPos = randomNum();
+            nodes[i].style.order = itemPos;
+        }
+    }
+}
+
+function sortCubes(){
+    for(let i = 0; i<nodes.length; i++){
+        if(nodes[i].className?.includes('cube')){
+            nodes[i].style.removeProperty('order');
+        }
+    }
+}
